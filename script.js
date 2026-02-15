0e23c3367e48005f77b0d16be1115477db4be0ec
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2026-02-15 17:31:08 +0000

    Refactor code using classes instead of just object constructors

--- script.js
+++ script.js
@@ -18,27 +18,64 @@ closeButton.addEventListener("click", () => {
     dialog.close();
 });
 
-function Book(title, author, pages, read) {
-  if (!new.target) {
-    throw Error("You must use the 'new operator to call the constructor!");
-  };
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.id = crypto.randomUUID();
-};
+class Book {
+    static generateID() {
+        return crypto.randomUUID();
+    };
 
-Book.prototype.toggleReadButton = function (readButton) {
-    this.read = !this.read;
-    if (this.read) {
-        readButton.textContent = "Unread";
-        readButton.classList.add("unread-book");
-        readButton.classList.remove("read-book");
-    } else {
-        readButton.textContent = "Read";
-        readButton.classList.remove("unread-book");
-        readButton.classList.add("read-book");
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = Book.generateID();
+    };
+
+     createReadButton() {
+        const readButton = document.createElement("button");
+        if (this.read === true) {
+            readButton.textContent = "Read";
+            readButton.classList.remove("unread-book");
+            readButton.classList.add("read-book");
+        } else {
+            readButton.textContent = "Unread";
+            readButton.classList.add("unread-book");
+            readButton.classList.remove("read-book");
+        };
+        readButton.addEventListener("click", () => {
+            this.toggleReadButton(readButton);
+        });
+        return readButton;
+    }
+    
+    toggleReadButton(readButton) {
+        this.read = !this.read;
+        if (this.read) {
+            readButton.textContent = "Read";
+            readButton.classList.remove("unread-book");
+            readButton.classList.add("read-book");
+        } else {
+            readButton.textContent = "Unread";
+            readButton.classList.add("unread-book");
+            readButton.classList.remove("read-book");
+        };
+    };
+
+    createDeleteButton() {
+        const deleteButton = document.createElement("button");
+        deleteButton.dataset.buttonID = this.id;
+        deleteButton.classList.add("delete-button");
+        const deleteButtonText = document.createTextNode("Delete Book");
+        deleteButton.appendChild(deleteButtonText);;
+
+        deleteButton.addEventListener("click", () => {
+            for (const row of table.rows) {
+                if (row.dataset.bookID === deleteButton.dataset.buttonID) {
+                    row.remove();
+                };
+            };
+        });
+        return deleteButton;
     };
 };
 
@@ -55,36 +92,9 @@ function updateCellContents(currentBook, newCell, newCellIndex) {
     } else if (newCellIndex === 2) {
         newCell.textContent = currentBook.pages;
     } else if (newCellIndex === 3) {
-        const readButton = document.createElement("button");
-        if (currentBook.read === true) {
-            readButton.textContent = "Unread";
-            readButton.classList.add("unread-book");
-            readButton.classList.remove("read-book");
-        } else {
-            readButton.textContent = "Read";
-            readButton.classList.remove("unread-book");
-            readButton.classList.add("read-book");
-        };
-        newCell.appendChild(readButton);
-
-        readButton.addEventListener("click", () => {
-            currentBook.toggleReadButton(readButton);
-        });
+        newCell.appendChild(currentBook.createReadButton());
     } else if (newCellIndex === 4) {
-        const deleteButton = document.createElement("button");
-        deleteButton.dataset.buttonID = currentBook.id;
-        deleteButton.classList.add("delete-button");
-        const deleteButtonText = document.createTextNode("Delete Book");
-        deleteButton.appendChild(deleteButtonText);
-        newCell.appendChild(deleteButton);
-
-        deleteButton.addEventListener("click", () => {
-            for (const row of table.rows) {
-                if (row.dataset.bookID === deleteButton.dataset.buttonID) {
-                    row.remove();
-                };
-            };
-        });
+        newCell.appendChild(currentBook.createDeleteButton());
     } else {
         return null;
     };
